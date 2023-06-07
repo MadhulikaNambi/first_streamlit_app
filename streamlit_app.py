d111daec707569c7c087d09781964bacc711565e
MadhulikaNambi <121902630+MadhulikaNambi@users.noreply.github.com>
2023-06-07 10:04:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ streamlit.write('Thanks for adding ', fruit_choice)
 
 my_cur.execute("insert into fruit_load_list values('from streamlit')");
 
-from urllib.error URLERROR
+from urllib.error URLError
